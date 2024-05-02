cc34143463d88e30f7788bfea59183cedfcfabbb
Jonas Peitz <jonaspeitz@proton.me>
2024-05-02 22:12:48 +0200

    added configs

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,6 +5,32 @@ use pest_derive::Parser;
 #[grammar = "conf.pest"]
 pub struct CONFParser;
 
-enum CONFValue {}
+enum CONFValue {
+    Object(Vec<(&'a str, CONFValue<'a>)>),
+    Array(Vec<CONFValue<'a>>),
+    Char(&'a str),
+    // could be an miskake
+    //
+    //this too
+    Path(CONFValue<'a>),
+    Wallpaper(CONFValue<'a>),
+    Color(CONFValue<'a>),
+    Cmd(CONFValue<'a>),
+    Profile(Vec<CONFValue<'a>>),
+    Name(&'a str),
+    String(&'a str),
+    Val(&'a str),
+    Value(&'a str),
+    Path(CONFValue<'a>),
+    Params(Vec<CONFValue<'a>>),
+    File(Vec<CONFValue<'a>>),
+    Import(CONFValue<'a>),
+    Exec(Vec<CONFValue<'a>>),
+    Script(Vec<CONFValue<'a>>),
+    Variable(CONFValue<'a>, CONFValue<'a>),
+    Comment,
+    Args(CONFValue<'a>),
+    Out(CONFValue<'a>),
+}
 
 pub fn parse_conf(path: &str) -> Result<Conf, String> {}
